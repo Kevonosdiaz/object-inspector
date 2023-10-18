9ce96ccbdfb3af2bbdeba081340395cdbc2cb128
Kevin Tran <91237886+Kevonosdiaz@users.noreply.github.com>
2023-10-17 22:52:53 -0600

    Add functionality for inspecting most elements of an object

---
+++ notes.md
@@ -0,0 +1,37 @@
+       // Store all the code that outputs each required piece of information in another method
+        // This method should act as a driver, and handle recursive calling (?)
+        // Note we can exclude anything with java.* for our assignment
+        // Also make modifiers human-readable using Method methods
+        // Watch out for repeat recursion (infinite loop)
+        // => HashSet of class names (since we are inspecting classes in our case)
+        // Check not null (?)
+        // => Don't need to check classes which are set to null
+        // => Reference the test cases!!!
+
+//        Method[] methods = objectClass.getMethods();
+//        System.out.println("\nPublic and Inherited Methods:");
+//        System.out.println(Arrays.toString(methods));
+//
+//        Method[] declaredMethods = objectClass.getDeclaredMethods();
+//        System.out.println("\nDeclared Methods:");
+//        System.out.println(Arrays.toString(declaredMethods));
+//
+//        // Working with Constructors
+//
+//        Constructor[] constructors = objectClass.getConstructors();
+//        System.out.println("\nPublic Constructors:");
+//        System.out.println(Arrays.toString(constructors));
+//
+//        Constructor[] declaredConstructors = objectClass.getDeclaredConstructors();
+//        System.out.println("\nDeclared Constructors:");
+//        System.out.println(Arrays.toString(declaredConstructors));
+//
+//        // Working with Fields
+//
+//        Field[] fields = objectClass.getFields();
+//        System.out.println("\nPublic and Inherited Fields:");
+//        System.out.println(Arrays.toString(fields));
+//
+//        Field[] declaredFields = objectClass.getDeclaredFields();
+//        System.out.println("\nDeclared Fields:");
+//        System.out.println(Arrays.toString(declaredFields));
--- src/Inspector.java
+++ src/Inspector.java
@@ -1,47 +1,76 @@
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
+import java.lang.reflect.InaccessibleObjectException;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.Arrays;
+import java.lang.reflect.Modifier;
 import java.util.HashSet;
 
 public class Inspector {
-    private static HashSet<String> seen; // Store name of already seen classes
+    private static final HashSet<String> seen = new HashSet<>(); // Store name of already seen classes
     public void inspect(Object obj, boolean recursive) {
-        // Store all the code that outputs each required piece of information in another method
-        // This method should act as a driver, and handle recursive calling (?)
-        // Note we can exclude anything with java.* for our assignment
-        // Also make modifiers human-readable using Method methods
-        // Watch out for repeat recursion (infinite loop)
-        // => HashSet of class names (since we are inspecting classes in our case)
-        // Check not null (?)
-        // => Don't need to check classes which are set to null
-        // => Reference the test cases!!!
-//        Method[] methods = objectClass.getMethods();
-//        System.out.println("\nPublic and Inherited Methods:");
-//        System.out.println(Arrays.toString(methods));
-//
-//        Method[] declaredMethods = objectClass.getDeclaredMethods();
-//        System.out.println("\nDeclared Methods:");
-//        System.out.println(Arrays.toString(declaredMethods));
-//
-//        // Working with Constructors
-//
-//        Constructor[] constructors = objectClass.getConstructors();
-//        System.out.println("\nPublic Constructors:");
-//        System.out.println(Arrays.toString(constructors));
-//
-//        Constructor[] declaredConstructors = objectClass.getDeclaredConstructors();
-//        System.out.println("\nDeclared Constructors:");
-//        System.out.println(Arrays.toString(declaredConstructors));
-//
-//        // Working with Fields
-//
-//        Field[] fields = objectClass.getFields();
-//        System.out.println("\nPublic and Inherited Fields:");
-//        System.out.println(Arrays.toString(fields));
-//
-//        Field[] declaredFields = objectClass.getDeclaredFields();
-//        System.out.println("\nDeclared Fields:");
-//        System.out.println(Arrays.toString(declaredFields));
+        Class<?> classObj = obj.getClass();
+        String className = classObj.getName();
+        // Stop once we get to Object level, or if we've already inspected this class
+        if (className.startsWith("java.lang") || seen.contains(className)) return;
+
+        // Name of declaring class
+        System.out.println("Declaring Class:\t" + className);
+
+        // Name of superclass
+        Class<?> superClass = classObj.getSuperclass();
+        String superClassName = superClass.getName();
+        System.out.println("Superclass:\t" + superClassName); // May be "void" (?)
+
+        // Name of interfaces
+        Class<?>[] interfaces = classObj.getInterfaces();
+        System.out.println("Interfaces:\n");
+        for (Class<?> inf : interfaces) {
+            System.out.println("\t" + inf.getName());
+        }
+        System.out.println("\n");
+
+        // Methods
+        Method[] methods = classObj.getDeclaredMethods();
+        System.out.println("Methods:\n");
+
+        for (Method method : methods) {
+            try {
+                method.setAccessible(true);
+                System.out.println("\t‣" + method.getName());
+                System.out.println("\t\t‣Exceptions Thrown:");
+                Class[] exceptions = method.getExceptionTypes();
+                for (Class<?> exception : exceptions) {
+                    System.out.println("\t\t\t" + exception.getName());
+                }
+
+                System.out.print("\t\t‣Parameter Types: (");
+                Class[] params = method.getParameterTypes()
+                System.out.println(")");
+                // Return type
+                System.out.println("\t\t‣Return Type: ");
+                System.out.println(method.getReturnType().getName());
+                // Modifiers
+                int mod = method.getModifiers();
+                System.out.println(Modifier.toString(mod));
+            } catch (InaccessibleObjectException e) {
+                System.out.println("Method not accessible");
+            }
+            ;
+        }
+
+        // Consider using a separate method for this
+
+        // Constructors
+
+
+        // Fields
+        // Consider using a separate method for this
+
+
+        // Check recursive flag
+
+
+
+
+        seen.add(className);
     }
 }
