79663924264bcae08fc4bfc7df31634bbcad8f7a
Kevin Tran <91237886+Kevonosdiaz@users.noreply.github.com>
2023-10-20 17:48:39 -0600

    Comment methods and perform extract method refactoring

--- src/Inspector.java
+++ src/Inspector.java
@@ -33,6 +33,25 @@ public class Inspector {
 
         // Current values of fields (specific to object)
         Field[] fields = classObj.getDeclaredFields();
+        printCurrFields(obj, recursive, fields);
+        System.out.print("\n");
+        while (!traversalQueue.isEmpty()) {
+            System.out.println("Printing from higher up the hierarchy!");
+            inspectClass(traversalQueue.removeLast());
+        }
+
+        if (recursive && !recurseQueue.isEmpty()) {
+            System.out.println("Printing field objects (since recursive flag was enabled)");
+            while (!recurseQueue.isEmpty()) {
+                System.out.println("\nPrinting object recursively!");
+                inspect(recurseQueue.removeLast(), recursive);
+            }
+        }
+
+    }
+
+    // Print out details for field values in obj
+    private void printCurrFields(Object obj, boolean recursive, Field[] fields) {
         if (fields.length == 0) {
             System.out.println("Field Values: None (no fields)");
         } else {
@@ -66,22 +85,9 @@ public class Inspector {
                 }
             }
         }
-        System.out.print("\n");
-        while (!traversalQueue.isEmpty()) {
-            System.out.println("Printing from higher up the hierarchy!");
-            inspectClass(traversalQueue.removeLast());
-        }
-
-        if (recursive && !recurseQueue.isEmpty()) {
-            System.out.println("Printing field objects (since recursive flag was enabled)");
-            while (!recurseQueue.isEmpty()) {
-                System.out.println("\nPrinting object recursively!");
-                inspect(recurseQueue.removeLast(), recursive);
-            }
-        }
-
     }
 
+    // Print out details for field values which are arrays
     public void printArray(Object obj, Class<?> objClass, boolean recursive) {
         Class<?> compType = objClass.componentType();
         System.out.println(compType);
@@ -102,6 +108,7 @@ public class Inspector {
 
     }
 
+    // Properly check if we have a primitive class that is wrapped (in case of auto-boxing)
     // From https://stackoverflow.com/questions/709961/determining-if-an-object-is-of-primitive-type
     public static boolean isWrapperType(Class<?> clazz) {
         return clazz.equals(Boolean.class) ||
@@ -114,6 +121,7 @@ public class Inspector {
                 clazz.equals(Float.class);
     }
 
+    // Print out details for a class
     private void inspectClass(Class<?> classObj) {
         String className = classObj.getName();
         if (className.startsWith(EXCLUDE_NAME) || seen.contains(className)) {
@@ -148,6 +156,80 @@ public class Inspector {
 
         // Methods
         Method[] methods = classObj.getDeclaredMethods();
+        printMethodDetails(methods);
+
+        // Constructors
+        Constructor<?>[] constructors = classObj.getConstructors();
+        printConstructorDetails(constructors);
+
+        // Fields
+        Field[] fields = classObj.getDeclaredFields();
+        printFieldDetails(fields);
+
+        // Ensure we do not infinitely inspect class
+        seen.add(className);
+    }
+
+    // Separately print details for all fields of a class
+    private static void printFieldDetails(Field[] fields) {
+        if (fields.length == 0) {
+            System.out.println("Fields: None");
+        } else {
+            System.out.println("Fields: ");
+            for (Field field : fields) {
+                try {
+                    field.setAccessible(true);
+                    System.out.println("\t- " + field.getName());
+
+                    // Field type
+                    System.out.print("\t\t- Field Type: ");
+                    System.out.println(field.getType().toString());
+                    // Modifiers
+                    int mod = field.getModifiers();
+                    System.out.println("\t\t- Modifiers: " + Modifier.toString(mod));
+                } catch (InaccessibleObjectException e) {
+                    System.out.println("Field not accessible");
+                }
+            }
+        }
+    }
+
+    // Separately print out details for all constructors of a class
+    private static void printConstructorDetails(Constructor<?>[] constructors) {
+        if (constructors.length == 0) {
+            System.out.println("Constructors: None");
+        } else {
+            try {
+                System.out.println("Constructors:");
+                for (Constructor<?> constructor : constructors) {
+                    constructor.setAccessible(true);
+
+                    // Name
+                    System.out.println("- Name: " + constructor.getName());
+
+                    // Parameters
+                    Class<?>[] params = constructor.getParameterTypes();
+                    if (params.length == 0) {
+                        System.out.println("\t- Parameter Types: None");
+                    } else {
+                        System.out.print("\t- Parameter Types: (");
+                        for (int i = 0; i < params.length-2; i++) {
+                            System.out.print(params[i].getName() + ", ");
+                        }
+                        System.out.println(params[params.length-1].getName() + ")");
+                    }
+                    // Modifiers
+                    int mod = constructor.getModifiers();
+                    System.out.println("\t- Modifiers: " + Modifier.toString(mod) + "\n");
+                }
+            } catch (InaccessibleObjectException e) {
+                System.out.println("Constructor not accessible");
+            }
+        }
+    }
+
+    // Separately print out all details of the methods of a class
+    private static void printMethodDetails(Method[] methods) {
         if (methods.length == 0) {
             System.out.println("Methods: None");
         } else {
@@ -190,63 +272,5 @@ public class Inspector {
                 }
             }
         }
-
-        // Constructors
-        Constructor<?>[] constructors = classObj.getConstructors();
-        if (constructors.length == 0) {
-            System.out.println("Constructors: None");
-        } else {
-            try {
-                System.out.println("Constructors:");
-                for (Constructor<?> constructor : constructors) {
-                    constructor.setAccessible(true);
-
-                    // Name
-                    System.out.println("- Name: " + constructor.getName());
-
-                    // Parameters
-                    Class<?>[] params = constructor.getParameterTypes();
-                    if (params.length == 0) {
-                        System.out.println("\t- Parameter Types: None");
-                    } else {
-                        System.out.print("\t- Parameter Types: (");
-                        for (int i = 0; i < params.length-2; i++) {
-                            System.out.print(params[i].getName() + ", ");
-                        }
-                        System.out.println(params[params.length-1].getName() + ")");
-                    }
-                    // Modifiers
-                    int mod = constructor.getModifiers();
-                    System.out.println("\t- Modifiers: " + Modifier.toString(mod) + "\n");
-                }
-            } catch (InaccessibleObjectException e) {
-                System.out.println("Constructor not accessible");
-            }
-        }
-
-        // Fields
-        Field[] fields = classObj.getDeclaredFields();
-        if (fields.length == 0) {
-            System.out.println("Fields: None");
-        } else {
-            System.out.println("Fields: ");
-            for (Field field : fields ) {
-                try {
-                    field.setAccessible(true);
-                    System.out.println("\t- " + field.getName());
-
-                    // Field type
-                    System.out.print("\t\t- Field Type: ");
-                    System.out.println(field.getType().toString());
-                    // Modifiers
-                    int mod = field.getModifiers();
-                    System.out.println("\t\t- Modifiers: " + Modifier.toString(mod));
-                } catch (InaccessibleObjectException e) {
-                    System.out.println("Field not accessible");
-                }
-            }
-        }
-
-        seen.add(className);
     }
 }
